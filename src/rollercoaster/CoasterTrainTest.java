56d9cd7c8a898329654d762c9988a5e7a22f9991
Snape <cjj741616831@gmail.com>
2015-10-31 17:52:05 -0400

    Sync

---
+++ src/rollercoaster/CoasterTrainTest.java
@@ -0,0 +1,19 @@
+package rollercoaster;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CoasterTrainTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
