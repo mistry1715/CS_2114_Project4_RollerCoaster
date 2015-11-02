592fe58a138e57a13e7c8273d851cda5d572779d
Snape <cjj741616831@gmail.com>
2015-11-02 18:44:55 -0500

    Sync

--- src/rollercoaster/CoasterWindow.java
+++ src/rollercoaster/CoasterWindow.java
@@ -30,65 +30,65 @@ public class CoasterWindow {
     private TextShape errorM;
     private TextShape coasterCount;
     private TextShape queueFront;
-    
+
     public CoasterWindow(RollerCoasterQueue queue) {
         this.queue = queue;
-        
+
         // Window
         window = new Window("Roller Coaster Ride");
-        
+
         // Add circle to the list
         partyCircles = new AList<CircleShape>();
         WaitingParty[] parties = new WaitingParty[queue.toArray().length];
-        
+
         for (int i = 0; i < parties.length; i++) {
             parties[i] = (WaitingParty) queue.toArray()[i];
         }
-        
+
         for (int i = 0; i < parties.length; i++) {
             CircleShape tempCircle = new CircleShape(parties[i].getLength() * DISPLAY_FACTOR,
                     parties[i].getLength() * DISPLAY_FACTOR);
-            
+
             if (parties[i].willSplit) {
                 tempCircle.setBackgroundColor(Color.GREEN);
             }
             else {
                 tempCircle.setBackgroundColor(Color.RED);
             }
-            
+
             partyCircles.add(tempCircle);
         }
-        
+
         // Button  
-        seatParty = new Button("Seat Party");
-        sendTrain = new Button("Send Train");
-        window.addButton(seatParty, WindowSide.SOUTH);
-        window.addButton(sendTrain, WindowSide.SOUTH);
-        
-        seatParty.onClick(this, "clickedSeatParty");
-        sendTrain.onClick(this, "clickedSendTrain");
-       
+        initSeatPartyButtonStatus();
+        initSendTrainButtonStatus();
+
+        // Separator
+        separator = new Shape(2, (int) (window.getGraphPanelHeight() * 0.8));
+        separator.setX(window.getGraphPanelWidth() / 5);
+        separator.setY(window.getGraphPanelHeight() / 10);
+        window.addShape(separator);
+
         // TextShape
-        coasterStatus = addTextShape("Coaster Status", 50, 50);
-        queueStatus = addTextShape("Queues Status:(Green:Willing to split)"
-                + "(Red:Not willing to split)", 0, 0);
-        errorM = addTextShape("Ride closed", 0, 0);
-        errorM.setBackgroundColor(Color.YELLOW);
-        coasterCount = addTextShape("Available Seats:", 0, 0);
-        queueFront = addTextShape("Perons in the first Waiting party:", 0, 0);
-        
-        window.addShape(coasterStatus);
-        window.addShape(queueStatus);
-        window.addShape(errorM);
-        window.addShape(coasterCount);
-        window.addShape(queueFront);
-        
+        displayCoasterCount();
+        displayQueueFront();
+
+        // Coaster
+        coaster = new Shape(10, TRAIN_HEIGHT);
+        window.addShape(coaster);
+        coaster.setX((separator.getX()/2) - 5);
+        coaster.setY((window.getGraphPanelHeight() - TRAIN_HEIGHT)/2);
+
+        updateCoaster();
+
+        // Queue
+        updateQueue();
     }
-    
+
     public void clickedSendTrain(Button b) {
         train.clear();
         updateCoaster();
-        
+
         if (queue.isEmpty()) {
             endRide();
         }
@@ -99,7 +99,7 @@ public class CoasterWindow {
 
     public void clickedSeatParty(Button b) {
         WaitingParty party = queue.dequeueParty(train.getOpenSeats());
-        
+
         if (party == null) {
             b.disable();
         }
@@ -108,7 +108,7 @@ public class CoasterWindow {
             updateQueue();
             updateCoaster();
         }
-        
+
         if (queue.isEmpty()) {
             b.disable();
         }
@@ -119,17 +119,109 @@ public class CoasterWindow {
     }
 
     private void updateCoaster() {
-        // TODO Auto-generated method stub
-        
-    }
-    
-    private void endRide() {
-        // TODO Auto-generated method stub
-        
+        coasterSeatsFilled = new Shape(10, 
+                (int) (2.5 * (CoasterTrain.SEATS - train.getOpenSeats())));
+        coasterSeatsFilled.setBackgroundColor(Color.YELLOW);
+        coasterSeatsFilled.setX(coaster.getX());
+        coasterSeatsFilled.setY((int) (coaster.getY() + train.getOpenSeats() * 2.5));
+        window.moveToFront(coasterSeatsFilled);
+
+        window.repaint();
+
+        updateTextShapes();
     }
 
     private void updateQueue() {
-        
+        updateCircles();    
+        updateTextShapes();
     }
-    
+
+    private void endRide() {
+        errorM = addTextShape("Ride closed",
+                (window.getGraphPanelWidth() - separator.getY() - errorM.getWidth()) / 2,
+                (window.getGraphPanelHeight() - errorM.getHeight()) / 2);
+        errorM.setBackgroundColor(Color.YELLOW);
+        window.addShape(errorM);
+
+        seatParty.disable();
+        sendTrain.disable();
+    }
+
+    private void drawCircles() {
+        int x = QUEUE_STARTX;
+        int y = QUEUE_STARTY;
+
+        for (int i = 0; i < partyCircles.getLength(); i++) {
+            CircleShape circle = partyCircles.getEntry(i);
+            circle.setX(x);
+            circle.setY(y - circle.getHeight() / 2);
+
+            x += circle.getWidth() + 5;
+        }
+
+        window.repaint();
+    }
+
+    private void updateCircles() {
+        drawCircles();
+    }
+
+    private void updateTextShapes() {
+        updateFirstParty(queue.getFront().getLength());
+
+        coasterCount.setText("Available Seats:" + train.getOpenSeats());
+
+        window.repaint();
+    }
+
+    private void updateFirstParty(int n) {
+        queueFront = addTextShape("Perons in the first Waiting party:" + n, 
+                separator.getX() + 20,
+                (int) (window.getGraphPanelHeight() * 0.9 - coasterCount.getHeight()));
+        window.addShape(queueFront);
+
+        window.repaint();
+    }
+
+    private void initSeatPartyButtonStatus() {
+        seatParty = new Button("Seat Party");
+        window.addButton(seatParty, WindowSide.SOUTH);
+        seatParty.onClick(this, "clickedSeatParty");
+        seatParty.enable();
+    }
+
+    private void initSendTrainButtonStatus() {
+        sendTrain = new Button("Send Train");
+        window.addButton(sendTrain, WindowSide.SOUTH);
+        sendTrain.onClick(this, "clickedSendTrain");
+        sendTrain.enable();
+    }
+
+    private void displayCoasterCount() {
+        coasterCount = addTextShape("Available Seats:" + train.getOpenSeats(), 
+                (separator.getX() - coasterStatus.getWidth()) / 2, 
+                (int) (window.getGraphPanelHeight() * 0.9 - coasterCount.getHeight()));
+        window.addShape(coasterCount);
+
+        coasterStatus = addTextShape("Coaster Status", 50, 50);
+        coasterStatus.setX((separator.getX() - coasterStatus.getWidth()) / 2);
+        coasterStatus.setY(separator.getY());
+        window.addShape(coasterStatus);
+
+        window.repaint();
+    }
+
+    private void displayQueueFront() {
+        queueFront = addTextShape("Perons in the first Waiting party:" + queue.getFront().getLength(), 
+                separator.getX() + 20,
+                (int) (window.getGraphPanelHeight() * 0.9 - coasterCount.getHeight()));
+        window.addShape(queueFront);
+
+        queueStatus = addTextShape("Queues Status:(Green:Willing to split)"
+                + "(Red:Not willing to split)", 0, 0);
+        window.addShape(queueStatus);
+
+        window.repaint();
+    }
+
 }
